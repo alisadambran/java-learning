b5af340d96e5d7fb0ebdfecd7ecebbdaf060a2ee
roman.chuvilev <fox761@gmail.com>
2025-04-16 21:18:37 +0400

    [feauture/lesson_2]: Classroom progress

--- src/main/java/lv/acodemy/SecondLesson.java
+++ src/main/java/lv/acodemy/SecondLesson.java
@@ -135,5 +135,15 @@ public class SecondLesson {
         System.out.println("Summa: " + summa);
 
 
+        int randomNumber = new Random
+        System.out.println(randomNumber);
+        int guess;
+        Scanner myScanner = new Scanner(System.in);
+        do {
+            System.out.println("Guess number from 1 to 10");
+            guess = myScanner.nextInt();
+        } while (guess != randomNumber);
+        System.out.println("Guessed!");
+
             }
 }
